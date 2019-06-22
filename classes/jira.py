703bb2272671ba342bd7d63163b60b4c77527b78
Dobry_kot <dputilin@moysklad.ru>
2019-06-22 22:03:09 +0300

    update jira class

--- classes/jira.py
+++ classes/jira.py
@@ -1,24 +1,36 @@
 from jira import JIRA
 from jira.exceptions import JIRAError
-import requests
+import requests, os, yaml, time
 
 class jira():
 
     def __init__(self):
         pass
 
-    def auth(self, url, **auth_data):
+    def auth(self):
+        PWD = os.getenv("HOME")
+  
+        with open("%s/.python_auth_cfg.yml" % PWD, 'r') as auth:
 
-        self.url            = url 
-        self.jira_options   = {
-                                  'server'        : url,
-                                  'max_retries'   : 1
-                              } 
+            auth_conf = yaml.load(auth, Loader=yaml.FullLoader)
+
+        jira_conf            = auth_conf['jira']
+        jira_admin_user      = jira_conf['admin_user']
+        jira_admin_api_token = jira_conf['admin_api_token']
+        jira_basic_url       = jira_conf['jira_basic_url']
+        
+        self.jira_basic_url  = jira_basic_url 
+        self.jira_options    = {
+                                   'server'        : jira_basic_url,
+                                   'max_retries'   : 1
+                               } 
         try:
-            self._session = JIRA(self.jira_options, basic_auth=(auth_data['auth'][0], auth_data['auth'][1]))
+            self._session = JIRA(self.jira_options, basic_auth=(jira_admin_user, jira_admin_api_token))
 
         except JIRAError and TypeError and KeyError as error:
-            print(error)
+            print('jira.init.auth <%s>' % error)
+            
+        return self._session
 
     def user_invite(self, email):
 
@@ -35,8 +47,8 @@ class jira():
         try:
             DELETE_USER = self._session.delete_user(username)
 
-        except JIRAError and TypeError and KeyError as error:
-            print('jira.init.user_delete <%s>' % error)
+        except JIRAError as error:
+            print('jira.init.user_delete <%s>' % error.text)
 
     def user_blocked(self, username):
         DEACTIVATE_USER = self._session.deactivate_user(username)
@@ -47,13 +59,25 @@ class jira():
         except JIRAError and TypeError and KeyError as error:
             print('jira_api_user.init.user_blocked <%s>' % error)
 
-    def user_add_group(self, group_list):
+    def user_add_group(self, groupName):
 
-        for group in group_list:
-            
-            try:
-                self._session.add_user_to_group(self.USERNAME , group)
+        time.sleep(15)
+        with open("config/jira_group_access.yml", 'r') as jira_group_access:
+            jira_group_access = yaml.load(jira_group_access, Loader=yaml.FullLoader)
 
-            except JIRAError and TypeError and KeyError as error:
-                print('jira_api_user.init.user_add_group <%s>' % error)
+        try:
+            group_list = jira_group_access[groupName]
+
+            for group in group_list:
+                
+                try:
+                    self._session.add_user_to_group(self.USERNAME , group)
+
+                except JIRAError and TypeError and KeyError as error:
+                    print('jira_api_user.init.user_add_group <%s>' % error)
+
+        except KeyError as error:
+            print('init.user_add_group | add default group | Error < groupName: %s is not defined >' % groupName)
+
+        
           
